149a3fbf0b81886f3acbfb8ff337b4756ac86d66
arielmygit <arielguerrero99@aragon.unam.mx>
2024-05-09 14:53:06 -0600

    Conectando a mysql y añadiendo tabla a la web

--- registro.php
+++ registro.php
@@ -12,6 +12,21 @@
     <link rel="stylesheet" href="styles.css">
     <link rel="icon" type="image/x-icon" href="/media/favicon2.png">
 
+    <style type="text/css">
+     
+.form_tabla {
+margin: 40px auto;
+padding: 20px;
+  border-radius: 8px;
+  max-width: 800px;
+  align-items: center;
+  align-items: center;
+     }
+    
+
+
+   </style>
+
 </head>
 
 <body>
@@ -135,8 +150,104 @@
     <script type="text/javascript" src="js/materialize.min.js"></script>
 
 
-
+    
 
 </body>
 
 </html>
+
+
+<?php
+
+$user = "root";
+$pass = "";
+$host = "localhost";
+
+
+$connection = mysqli_connect($host, $user, $pass);
+
+
+$nombre = $_POST["nombre"] ;
+$ap_p = $_POST["ap_paterno"] ;
+$ap_m = $_POST["ap_materno"] ;
+$email = $_POST["email"] ;
+$celular = $_POST["celular"] ;
+
+
+
+
+if(!$connection) 
+        {
+            // echo "<h4>No se ha podido conectar con el servidor</h4>" . mysql_error();
+        }
+  else
+        {
+            // echo "<b><h4>Hemos conectado al servidor</h4></b>" ;
+        }
+       
+        $datab = "prueba1";
+        
+        $db = mysqli_select_db($connection,$datab);
+
+        if (!$db)
+        {
+        // echo "No se ha podido encontrar la Tabla";
+        }
+        else
+        {
+        // echo "<h4>Tabla seleccionada:</h4>" ;
+        }
+        
+        $instruccion_SQL = "INSERT INTO formulario 
+        (nombre,ap_p,ap_m,email,celular)
+            VALUES(
+                '$nombre',
+                '$ap_p',
+                '$ap_m',
+                '$email',
+                '$celular')";
+                           
+                            
+        $resultado = mysqli_query($connection,$instruccion_SQL);
+
+        
+        $consulta = "SELECT * FROM formulario";
+        
+$result = mysqli_query($connection,$consulta);
+
+if(!$result) 
+{
+    echo "No se ha podido realizar la consulta";
+}
+echo "<table class='form_tabla highlight'>";
+echo "<thead>";
+echo "<tr>";
+echo "<th>>id</th>";
+echo "<th>Nombre</th>";
+echo "<th>Apellido Paterno</th>";
+echo "<th>Apellido Materno</th>";
+echo "<th>Email</th>";
+echo "<th>Celular</th>";
+echo "</tr>";
+echo "</thead>";
+
+while ($colum = mysqli_fetch_array($result))
+ {
+    echo "<tbody>";  
+    echo "<tr>";
+    echo "<td>" . $colum['id']. "</td>";
+    echo "<td>" . $colum['nombre']. "</td>";
+    echo "<td>" . $colum['ap_p'] . "</td>";
+    echo "<td>" . $colum['ap_m'] . "</td>";
+    echo "<td>" . $colum['email'] . "</td>>";
+    echo "<td>" . $colum['celular'] . "</td>";
+    echo "</tr>";
+}
+echo "</tbody>";
+echo "</table>";
+
+mysqli_close( $connection );
+   echo'<a href="index.html"> Volver Atrás</a>';
+
+
+?>
---
+++ tabla.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE `formulario`(
+
+`id`  int(5) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+`nombre` varchar(20) not null,
+`ap_p` varchar(15) not null,
+`ap_m` varchar(15) not null,
+`email` varchar(20) not null,
+`celular` varchar(15) not null
+); engine=Innodb default charset=utf8;
